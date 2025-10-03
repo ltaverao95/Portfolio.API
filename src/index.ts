9c132a1246934cd7ecd69e0ccf4d1c0a2d2b1479
luistaveraflyr <luis.tavera@newshore.es>
2025-10-03 15:27:51 -0500

    refactor: enhance blog validation and improve JSON request size limit

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,12 @@ dotenv.config();
 
 const app = express();
 
+app.use(
+ express.json({
+    limit: '20mb',
+ }),
+);
+
 app.use('/api', blogRoutes);
 
 const port = parseInt(process.env.PORT || '3000');
