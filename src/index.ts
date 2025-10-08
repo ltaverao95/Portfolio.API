18138853a80e8e97b49e211eb307cc38a640566c
luistaveraflyr <luis.tavera@newshore.es>
2025-10-08 18:27:26 -0500

    fix: change console.log to console.warn for improved logging clarity

--- src/index.ts
+++ src/index.ts
@@ -39,5 +39,5 @@ if(process.env.NODE_ENV === 'production') {
 
 const port = parseInt(process.env.PORT || '3000');
 app.listen(port, () => {
-  console.log(`listening on port ${port} `);
+  console.warn(`listening on port ${port} `);
 });
